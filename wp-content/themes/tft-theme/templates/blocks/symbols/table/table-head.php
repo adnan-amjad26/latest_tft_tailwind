c8b3fb72e9bdd4ef8ee80671d88b735eb91ff300
adnan-amjad26 <adnanamjad26@gmail.com>
2023-11-28 15:25:28 +0500

    changes for the gitignore

--- wp-content/themes/tft-theme/templates/blocks/symbols/table/table-head.php
+++
@@ -1,8 +0,0 @@
-<tr class="font-wadik text-[14px] border-b border-white xl:border-black">
-	<th class="whitespace-nowrap pb-[25px] pr-[38px] text-left xl:pr-[24px]"><?= $symbol; ?></th>
-	<th class="whitespace-nowrap pb-[25px] px-[38px] xl:px-6"><?= $bid_price; ?></th>
-	<th class="whitespace-nowrap pb-[25px] px-[38px] xl:px-6"><?= $ask_price; ?></th>
-	<th class="whitespace-nowrap pb-[25px] px-[38px] xl:px-6"><?= $spread; ?></th>
-	<th class="whitespace-nowrap pb-[25px] px-[38px] xl:px-6"><?= $simulated_comissions; ?></th>
-	<th class="whitespace-nowrap pb-[25px] px-[38px] xl:px-6"><?= $overall_costs; ?></th>
-</tr>
